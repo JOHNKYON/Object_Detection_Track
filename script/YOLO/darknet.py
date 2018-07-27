de1eb350ad5b2eb301df52a24ef4c73b22b4cb94
JOHNKYON <zhaofengbo1321@163.com>
2018-07-27 20:27:30 +0900

    Finish forward passing part

--- script/YOLO/darknet.py
+++ script/YOLO/darknet.py
@@ -6,6 +6,8 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 
+from script.YOLO.util import *
+
 
 def parse_cfg(cfgfile):
     """
@@ -153,5 +155,165 @@ class DetectionLayer(nn.Module):
         self.anchors = anchors
 
 
-blocks = parse_cfg("cfg/yolov3.cfg")
-print(create_modules(blocks))
+# blocks = parse_cfg("cfg/pythyolov3.cfg")
+# print(create_modules(blocks))
+
+
+class Darknet(nn.Module):
+    def __init__(self, cfgfile):
+        super(Darknet, self).__init__()
+        self.blocks = parse_cfg(cfgfile)
+        self.net_info, self.module_list = create_modules(self.blocks)
+
+    def forward(self, x, cuda):
+        modules = self.blocks[1:]
+        outputs = {}
+
+        write = 0
+        for i, module in enumerate(modules):
+            module_type = (module["type"])
+
+            if module_type == "convolutional" or module_type == "upsample":
+                x = self.module_list[i](x)
+
+            elif module_type == "route":
+                layers = module["layers"]
+                layers = [int(a) for a in layers]
+
+                if layers[0] > 0:
+                    layers[0] = layers[0] - i
+
+                if len(layers) == 1:
+                    x = outputs[i + (layers[0])]
+
+                else:
+                    if layers[1] > 0:
+                        layers[1] = layers[1] - i
+
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+
+                    x = torch.cat((map1, map2), 1)
+
+            elif module_type == "shortcut":
+                from_ = int(module["from"])
+                x = outputs[i-1] + outputs[i+from_]
+
+            elif module_type == "yolo":
+                anchors = self.module_list[i][0].anchors
+                # Get the input dimensions
+                inp_dim = int (self.net_info["height"])
+
+                # Get the number of classes
+                num_classes = int(module["classes"])
+
+                # Transform
+                x = x.data
+                x = predict_transform(x, inp_dim, anchors, num_classes, cuda)
+
+                if not write:
+                    detections = x
+                    write = 1
+                else:
+                    detections = torch.cat((detections, x), 1)
+
+            outputs[i] = x
+        return detections
+
+    def load_weights(self, weightfile):
+        # Open the weights file
+        fp = open(weightfile, 'rb')
+
+        header = np.fromfile(fp, dtype=np.int32, count=5)
+        self.header = torch.from_numpy(header)
+        self.seen = self.header[3]
+
+        weights = np.fromfile(fp, dtype=np.float32)
+
+        ptr = 0
+        for i in range(len(self.module_list)):
+            module_type = self.blocks[i+1]["type"]
+
+            # If convolution, load weights, ignore otherwise
+            if module_type == "convolutional":
+                model = self.module_list[i]
+                try:
+                    batch_normalize = int(self.blocks[i+1]["batch_normalize"])
+                except:
+                    batch_normalize = 0
+
+                conv = model[0]
+
+                if batch_normalize:
+                    bn = model[1]
+
+                    # Get the number of weights of Batch Norm Layer
+                    num_bn_biases = bn.bias.numel()
+
+                    # Load the weights
+                    bn_biases = torch.from_numpy(weights[ptr:ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_weights = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_mean = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_var = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    # Cast the loaded weights into dims of model weights.
+                    bn_biases = bn_biases.view_as(bn.bias.data)
+                    bn_weights = bn_weights.view_as(bn.weight.data)
+                    bn_running_mean = bn_running_mean.view_as(bn.running_mean)
+                    bn_running_var = bn_running_var.view_as(bn.running_var)
+
+                    # Copy the data to model
+                    bn.bias.data.copy_(bn_biases)
+                    bn.weight.data.copy_(bn_weights)
+                    bn.running_mean.copy_(bn_running_mean)
+                    bn.running_var.copy_(bn_running_var)
+
+                else:
+                    # Number of biases
+                    num_biases = conv.bias.numel()
+
+                    # Load the weights
+                    conv_biases = torch.from_numpy(weights[ptr: ptr + num_biases])
+                    ptr += num_biases
+
+                    # Reshape the loaded weights according to the dims of the model weights
+                    conv_biases = conv_biases.view_as(conv.bias.data)
+
+                    # Finally copy the data
+                    conv.bias.data.copy_(conv_biases)
+
+                # Load weights for convolutional layers
+                num_weights = conv.weight.numel()
+
+                conv_weights = torch.from_numpy(weights[ptr: ptr + num_weights])
+                ptr += num_weights
+
+                conv_weights = conv_weights.view_as(conv.weight.data)
+                conv.weight.data.copy_(conv_weights)
+
+
+# def get_test_input():
+#     img = cv2.imread("dog-cycle-car.png")
+#     img = cv2.resize(img, (416, 416))          #Resize to the input dimension
+#     img_ =  img[:,:,::-1].transpose((2,0,1))  # BGR -> RGB | H X W C -> C X H X W
+#     img_ = img_[np.newaxis,:,:,:]/255.0       #Add a channel at 0 (for batch) | Normalise
+#     img_ = torch.from_numpy(img_).float()     #Convert to float
+#     img_ = Variable(img_)                     # Convert to Variable
+#     return img_
+#
+#
+# model = Darknet("cfg/yolov3.cfg")
+# inp = get_test_input()
+# pred = model(inp, torch.cuda.is_available())
+# print(pred)
+
+model = Darknet("cfg/yolov3.cfg")
+model.load_weights("yolov3.weights")
+
---
+++ script/YOLO/test_parse_cfg.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+
+class TestParse_cfg(TestCase):
+    pass
